4f986825b31d1f8f56fe190fef24c88e9c5a7de1
Michael Sarchet <mikesarchet@gmail.com>
2014-01-28 10:36:09 -0800

    Initial Commit

---
+++ BundlerMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using BundlerMiddleware;
+
+namespace mist.Middleware
+{
+    using System.IO;
+    using System.Net;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using System.Web.Optimization;
+
+    using Microsoft.Owin;
+
+    public class BundlerMiddleware : OwinMiddleware
+    {    
+        public BundlerMiddleware(OwinMiddleware next) : base(next)
+        {
+        }
+
+        private async static Task<string> MatchReplacer(string path)
+        {
+            using (var stream = File.OpenText(path))
+            {
+                var file = await stream.ReadToEndAsync();
+                var matcher = new Regex(@"\!\!(scripts|styles):([^\}]+)\!\!", RegexOptions.Compiled);
+                return matcher.Replace(file, MatchReplace);
+            }
+        }
+
+        private static string MatchReplace(Match match)
+        {
+            return (match.Groups[1].Value == "scripts"
+                         ? Scripts.Render(match.Groups[2].Value)
+                         : Styles.Render(match.Groups[2].Value)).ToString();
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            var route = BundlerRoutes.Routes.FirstOrDefault(r => r.Route.Equals(context.Request.Path.ToString()));
+            if (route != null)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.Response.ContentType = "text/html";
+                var baseContext = context.Environment["System.Web.HttpContextBase"] as System.Web.HttpContextBase;
+                if (baseContext != null)
+                {
+                    var fullPath = baseContext.Server.MapPath(route.FilePath);
+                    await context.Response.WriteAsync(await MatchReplacer(fullPath));
+                }
+                else
+                {
+                    await Next.Invoke(context);
+                }
+            }
+            else
+            {
+                await Next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ BundlerRouteTables.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Web.Management;
+using System.Web.Routing;
+
+namespace BundlerMiddleware
+{
+    public class BundlerRoutes
+    {
+        public static BundlerRouteTable Routes = new BundlerRouteTable();
+    }
+
+    public class BundlerRouteTable : ICollection<BundlerRoute>
+    {
+        private ICollection<BundlerRoute> Routes = new List<BundlerRoute>();
+
+        public void Add(BundlerRoute item)
+        {
+            Routes.Add(item);
+        }
+
+        public void Clear()
+        {
+            Routes.Clear();
+        }
+
+        public bool Contains(BundlerRoute item)
+        {
+            return Routes.Contains(item);
+        }
+
+        public void CopyTo(BundlerRoute[] array, int arrayIndex)
+        {
+            Routes.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return Routes.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return Routes.IsReadOnly;  }
+        }
+
+        public bool Remove(BundlerRoute item)
+        {
+            return Routes.Remove(item);
+        }
+
+        public IEnumerator<BundlerRoute> GetEnumerator()
+        {
+            return Routes.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return Routes.GetEnumerator();
+        }
+    }
+
+    public class BundlerRoute
+    {
+        public string Route { get; set; }
+        public string FilePath { get; set; }
+
+        public BundlerRoute(string route, string filePath)
+        {
+            this.Route = route;
+            this.FilePath = filePath;
+        }
+
+        public static BundlerRoute BundlerRouteFromVirutalPath(string route, string virutalPath)
+        {
+            return new BundlerRoute(route, System.Web.VirtualPathUtility.ToAbsolute(virutalPath));
+        }
+    }
+}
