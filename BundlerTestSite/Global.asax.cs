5f327022730a554526ce4aefc1255faa62b24410
Michael Sarchet <mikesarchet@gmail.com>
2014-03-13 08:49:25 -0700

    Updates to 0.3.2

--- BundlerTestSite/Global.asax.cs
+++ BundlerTestSite/Global.asax.cs
@@ -1,11 +1,6 @@
 ﻿using BundlerMiddleware;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Optimization;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace BundlerTestSite
 {
