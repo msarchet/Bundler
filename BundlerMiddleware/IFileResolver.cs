aba85a9cc8718d68ea3971e24b77df09ffd5719a
Will Anderson <will@itsananderson.com>
2014-07-21 14:41:46 -0700

    Indent with 4 spaces

--- BundlerMiddleware/IFileResolver.cs
+++ BundlerMiddleware/IFileResolver.cs
@@ -2,17 +2,17 @@
 {
     using Microsoft.Owin;
 
-	/// <summary>
-	/// Used for resolving file paths to full paths
-	/// </summary>
+    /// <summary>
+    /// Used for resolving file paths to full paths
+    /// </summary>
     public interface IFileResolver
     {
-		/// <summary>
-		/// Gets the full file path for the route
-		/// </summary>
-		/// <param name="context">Request Context</param>
-		/// <param name="route">Route to resolve the path for</param>
-		/// <returns>The full file path</returns>
+        /// <summary>
+        /// Gets the full file path for the route
+        /// </summary>
+        /// <param name="context">Request Context</param>
+        /// <param name="route">Route to resolve the path for</param>
+        /// <returns>The full file path</returns>
         string GetFilePath(IOwinContext context, BundlerRoute route);
     }
 }
